d034b2caa4df875d2a01e86a7742b6723cb556d1
Niklas <198754647+BlankAccountsUsername@users.noreply.github.com>
2025-05-08 17:30:28 +0200

    wip add feed

---
+++ code/frontend/vogel-social-media/src/feed.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./feed.css";
+
+function Feed() {
+  return (
+    <div className="LandingPage">
+      <header className="LandingPage-header">
+        <p className="Header1" color="">
+          Welcome
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Feed;
