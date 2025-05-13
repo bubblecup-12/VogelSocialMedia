0125c88bee9c13461872074305045857114920f9
MisbehavedNinjaRadiator <120029998+MisbehavedNinjaRadiator@users.noreply.github.com.>
2025-05-13 15:59:43 +0200

    altes design mit svgs
    :wq

--- code/frontend/vogel-social-media/src/StartPage.tsx
+++ code/frontend/vogel-social-media/src/StartPage.tsx
@@ -4,6 +4,15 @@ import "./StartPage.css";
 function StartPage() {
   return (
     <div className="StartPage">
+        <div className="topContainer">
+          <img className="icon" src="/media/icons/vogel.svg" />
+          <p>Feather Feed</p>
+          <a>Page</a>
+          <a>Page</a>
+          <a>Page</a>
+          <button className="loginButton smallLoginButton">Login</button>
+          <img src="/media/icons/threeMenuBars.svg" className="icon"/>
+        </div>
         <div className="middleContainer">
             <h1 className="welcome">Welcome</h1>
             <p className="welcomeMessage">Exchange pictures and information about birds and be part of our big bird-loving community.</p>
