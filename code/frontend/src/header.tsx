583115d505e153231fd7c03b7c3d8d0edf526dd4
Niklas <198754647+BlankAccountsUsername@users.noreply.github.com>
2025-05-14 18:42:12 +0200

    add Header

---
+++ code/frontend/src/header.tsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+
+function Header() {
+
+  return (
+      <header className="base-header">
+        <div className="base-header-icon"> <img src='/assets/icons/feather_black.svg' alt="featherIcon" /> </div>
+        <p className="base-header-title">
+          Feather Feed
+        </p>
+        <div className="base-header-icon"> <img src="/assets/icons/three_menu_stripes_black.svg" alt="menuIcon" /> </div>
+      </header>
+  );
+}
+
+export default Header;
