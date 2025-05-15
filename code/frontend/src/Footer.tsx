bd6578f4ce13d294bc603aa2a57245566faf3275
MisbehavedNinjaRadiator <120029998+MisbehavedNinjaRadiator@users.noreply.github.com>
2025-05-15 18:05:28 +0200

    Added a header, all links except github only refresh the page. Also added header&footer to App and removed unneccessary login

---
+++ code/frontend/src/Footer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-left">
+        <a href="" className="footer-link feather">
+          <img src="/assets/icons/feather_black.svg" alt="Feather icon" />
+          <span>Feather Feed</span>
+        </a>
+        <a href="https://github.com" target="_blank " className="footer-link github">
+          <img src="/assets/icons/github.svg" alt="GitHub icon" />
+          <span>GitHub</span>
+        </a>
+      </div>
+
+      <div className="footer-right">
+        <div className="footer-column">
+          <a href="">Home</a>
+          <a href="">About</a>
+        </div>
+        <div className="footer-column">
+          <a href="">Login</a>
+          <a href="">Sign up</a>
+        </div>
+        <div className="footer-column">
+          <a href="">Create Post</a>
+          <a href="">Feed</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
