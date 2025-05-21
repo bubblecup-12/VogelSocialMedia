72d172424baf7a60c9b1643f97651f15227c5730
Ilay Eble <ilayeble@gmail.com>
2025-05-21 10:33:04 +0200

    Basic Login Screen Desktop (Just Login)

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,17 +1,11 @@
 import React, { use } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
-import Footer from './Footer';
+import Login from './Login';
 import Header from './header';
 
+
 function App() {
-  
   return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
+      <Login/>
   );
 }
 
