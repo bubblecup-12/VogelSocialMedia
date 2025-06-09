33d92a39ee2b797351470255b1cba4b68d7ccf1f
Ilay Eble <ilayeble@gmail.com>
2025-06-09 12:36:50 +0200

    Basic Login and Signup (Desktop)

--- code/frontend/src/Login.tsx
+++ code/frontend/src/Login.tsx
@@ -2,31 +2,38 @@ import React, { use } from 'react';
 import "./Login.css";
 import { useState, useEffect } from 'react';
 import Header from './header';
+import { url } from 'inspector';
 
 
 function Login(){
+    const toggleLogin=(event:React.MouseEvent<HTMLElement>)=>{event.preventDefault(); setSignup(!signup)};
+    const [signup, setSignup]= useState(false);
     return(
         <div className='login-display'>
             <Header></Header>
             <div className="login-login">
                 <div className='login-part'>
-                    <div className='login-image'></div>
+                    <div className={signup? 'signup-image': 'login-image'}></div>
                 </div>
                 <div className='login-part' >
                     <div className='login-text'>
-                        Login
+                        {signup? "Sign Up": "Login"}
                     </div>
                     <div className='login-div-input'>
-                        <img className= 'login-icon' src='\assets\icons\username_orange.svg'></img>
+                        <img className= 'login-icon' src='/assets/icons/email_orange.svg'></img>
                         <input type='text' className='login-input' placeholder='Username'></input>
                     </div>
+                    {signup? <div className='login-div-input'>
+                        <img className= 'login-icon' src='\assets\icons\username_orange.svg'></img>
+                        <input type='email' className='login-input' placeholder='Email'></input>
+                    </div> : ""}
                     
                     <div className='login-div-input'>
                         <img className= 'login-icon' src='\assets\icons\password_orange.svg'></img>
                         <input type='password' className='login-input'placeholder='Password'></input>
                     </div>
-                        <input type='button' className='login-button' value='Login'></input>
-                        <div className='login-signup'>Don't have an account yet? Click <a href='' className='login-link'>here</a> to sign up</div>
+                        <input type='button' className='login-button' value={signup? "Signup": "Login"}></input>
+                        <div className='login-signup'>{signup? "Already have an account?": "Don't have an account yet?"} Click <a href='' className='login-link' onClick={toggleLogin}>here</a> {signup? "login": "to sign up"}</div>
                 </div>
             </div>
         </div>
