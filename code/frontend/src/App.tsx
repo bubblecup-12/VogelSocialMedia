2fe625229cb7f017f2ec78c2a9eb0c966e417209
Ilay Eble <ilayeble@gmail.com>
2025-06-11 16:05:07 +0200

    Fixed link Login/Signup, cursor on button

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import LoginSignUpPage from './LoginAndSignUpPage';
-
+import React, { use } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+import Footer from './Footer';
+import Header from './header';
+import LoginAndSignUpPage from './LoginAndSignUpPage';
 
 function App() {
   return (
-      <LoginSignUpPage/>
+    <div className="App">
+      <LoginAndSignUpPage></LoginAndSignUpPage>
+    </div>
   );
 }
 
