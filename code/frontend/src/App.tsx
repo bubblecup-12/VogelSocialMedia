4face451f9a141772aa931dd077ad0caa9ba3197
MisbehavedNinjaRadiator <120029998+MisbehavedNinjaRadiator@users.noreply.github.com.>
2025-06-11 15:17:01 +0200

    Renamed footer and created folders

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -4,13 +4,13 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import Footer from './Footer';
 import Header from './header';
+import Feed from './feed/Feed';
 
 function App() {
   
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
+      <Feed/>
     </div>
   );
 }
---
+++ code/frontend/src/feed/Feed.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Header from "../header";
+import Footer from "../Footer";
+
+function Feed() {
+    return (
+        //<Header/>
+        <div className="feedContainer">
+
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Header/>
+            <Footer/>
+        </div>
+
+       // <Footer/>
+    );
+}
+
+export default Feed;
