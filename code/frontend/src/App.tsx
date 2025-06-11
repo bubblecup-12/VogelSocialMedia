26caaffa8e50803a134584d76d3505fced06333e
Ilay Eble <ilayeble@gmail.com>
2025-06-11 16:16:06 +0200

    Fix

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -2,15 +2,13 @@ import React, { use } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-import Footer from './Footer';
-import Header from './header';
+import LoginAndSignUpPage from './LoginAndSignUpPage';
 
 function App() {
   
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
+      <LoginAndSignUpPage/>
     </div>
   );
 }
