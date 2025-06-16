d43ae24ee8717c682d816d76cc4edc4064f27d78
luisa.bellitto <luisa.bellitto@bluehands.de>
2025-06-16 09:52:33 +0200

    first profile page test

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import LoginAndSignUpPage from './LoginAndSignUpPage';
+import Profile from './Profile';
+import Header from './header';
+import Test from './test';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <LoginAndSignUpPage/>
+      <Test />
+      <Profile/>
     </div>
   );
 }
