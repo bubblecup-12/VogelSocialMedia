7051f4dcf5ce64f85e0d386b2aa0a54146164a9c
Ilay Eble <ilayeble@gmail.com>
2025-06-18 08:05:38 +0200

    Initial Commit

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import LoginAndSignUpPage from './LoginAndSignUpPage';
+import PostCreation from './PostCreation';
 
 function App() {
   return (
     <div className="App">
-      <LoginAndSignUpPage/>
+      <PostCreation/>
     </div>
   );
 }
---
+++ code/frontend/src/PostCreation.tsx
@@ -0,0 +1,14 @@
+import Header from "./header";
+import "./postCreation.css";
+
+function PostCreation(){
+
+    return(
+    <div className="create-display">
+        <Header/>
+        <div className="create-part">
+            <h1>Create Post</h1>
+        </div>
+    </div>);
+}
+export default PostCreation;
