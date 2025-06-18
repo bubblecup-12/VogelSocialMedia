ff141a8f7e6ccf35dca8423b7e1bcc935f92f7c6
Niklas <198754647+BlankAccountsUsername@users.noreply.github.com>
2025-06-18 17:31:34 +0200

    add drawer menu

--- code/frontend/src/components/Header.tsx
+++ code/frontend/src/components/Header.tsx
@@ -1,7 +1,10 @@
 import "./header.css";
 import React, { useState, useRef, useEffect } from "react";
-import { Menu, MenuItem } from '@mui/material';
-
+import { Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, MenuItem, SwipeableDrawer } from '@mui/material';
+import Box from '@mui/material/Box';
+import InboxIcon from "@mui/icons-material/Inbox";
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // TODO: Dinge so umstrukturieren, dass der State für das offene menü in Header ist und das Menü auch in Header, sodass es mit width 100% die volle breite einnehmen kann
 
@@ -12,6 +15,37 @@ function Header() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+  const iconList = [InboxIcon, MailIcon, InboxIcon, MailIcon, InboxIcon, MailIcon];
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={() => setIsOpen(false)}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <div className="outest-container" >
@@ -22,7 +56,14 @@ function Header() {
         </p>
         <div className="header-icon"> {isOpen? <img src='/assets/icons/close_orange.svg' alt="close menu" ref={iconRef} onClick={toggleMenu}/> : <img src='/assets/icons/menu_orange.svg' alt="menu" onClick={toggleMenu}  />} </div>
       </header>
-      <Menu1 isOpen={isOpen} toggleMenu={toggleMenu} setIsOpen={setIsOpen} iconRef={iconRef}/> 
+      <SwipeableDrawer
+      anchor={"right"}
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      onOpen={() => setIsOpen(true)}
+    >
+      {DrawerList}
+    </SwipeableDrawer>
     </div>
   );
 }
