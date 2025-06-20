ad01ad5a1eaa99c2783688b18b09e5bbbe6d99d4
Kai Ritthaler <kajsu.kaepfer@gmial.com>
2025-06-20 12:40:47 +0200

    added react router and routes to login and signup

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -4,12 +4,25 @@ import "./styles/fonts.css";
 import LoginAndSignUpPage from "./pages/LoginAndSignUpPage";
 import Footer from "./components/Footer";
 import Header from "./components/header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <LoginAndSignUpPage />
+      <Router>
+        <Routes>
+          <Route
+            path="/login"
+            element={<LoginAndSignUpPage signupProp={false} />}
+          ></Route>
+          <Route
+            path="/register"
+            element={<LoginAndSignUpPage signupProp={true} />}
+          ></Route>
+        </Routes>
+      </Router>
+
       <Footer />
     </div>
   );
