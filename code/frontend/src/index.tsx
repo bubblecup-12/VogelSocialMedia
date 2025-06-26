f741967f900f77ae5d7c062e6675214296fc9ee2
Niklas <198754647+BlankAccountsUsername@users.noreply.github.com>
2025-06-26 10:49:27 +0200

    do injectfirst around app

--- code/frontend/src/index.tsx
+++ code/frontend/src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { StyledEngineProvider } from "@mui/material";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
