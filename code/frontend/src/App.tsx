8b70e900ac6955e1f0981c3b4649cf055fd73c12
Ilay Eble <ilayeble@gmail.com>
2025-06-26 10:54:30 +0200

    Configuration after merge

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -2,13 +2,12 @@ import React, { use } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-import LoginAndSignUpPage from './LoginAndSignUpPage';
-import PostCreation from './PostCreation';
+import LoginAndSignUpPage from './pages/LoginAndSignUpPage';
+import PostCreation from './pages/PostCreation';
 import "./App.css";
 import "./styles/colors.css";
 import "./styles/fonts.css";
 import "./styles/sizes.css";
-import LoginAndSignUpPage from "./pages/LoginAndSignUpPage";
 import Footer from "./components/Footer";
 import Header from "./components/header";
 import Profile from "./pages/Profile";
@@ -16,9 +15,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <PostCreation/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route
+            path="/login"
+            element={<LoginAndSignUpPage signupProp={false} />}
+          ></Route>
+          <Route
+            path="/register"
+            element={<LoginAndSignUpPage signupProp={true} />}
+          ></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/create" element={<PostCreation/>}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
