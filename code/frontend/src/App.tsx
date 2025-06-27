9f9a21818ab5a0d0ceb118f08cc5462bd74151fd
Kai Ritthaler <kajsu.kaepfer@gmial.com>
2025-06-27 08:59:23 +0200

    flappy Bird

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -8,32 +8,31 @@ import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Auth } from "./api/Auth";
-
-
-
+import { NotFound } from "./pages/404Page/NotFoundPage";
 
 function App() {
-	return (
-		<Auth>
-			<Router>
-				<div className="App">
-					<Header />
-					<Routes>
-						<Route
-							path="/login"
-							element={<LoginAndSignUpPage signupProp={false} />}
-						></Route>
-						<Route
-							path="/register"
-							element={<LoginAndSignUpPage signupProp={true} />}
-						></Route>
-						<Route path="/profile" element={<Profile />}></Route>
-					</Routes>
-					<Footer />
-				</div>
-			</Router>
-		</Auth>
-	);
+  return (
+    <Auth>
+      <Router>
+        <Header />
+        <div className="App">
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route
+              path="/login"
+              element={<LoginAndSignUpPage signupProp={false} />}
+            ></Route>
+            <Route
+              path="/register"
+              element={<LoginAndSignUpPage signupProp={true} />}
+            ></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </Auth>
+  );
 }
 
 export default App;
