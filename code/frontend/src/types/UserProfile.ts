f2f7a86c9ce21e9de3980f25c574c412855fb4ca
luisa.bellitto <luisa.bellitto@bluehands.de>
2025-06-27 13:55:08 +0200

    add image connection to database

---
+++ code/frontend/src/types/UserProfile.ts
@@ -0,0 +1,9 @@
+export type UserProfile = {
+    id: string;
+    username: string;
+    bio: string | undefined;
+    profilePictureUrl: string | null;
+    followers: number;
+    following: number;
+    // posts: number;
+  }
