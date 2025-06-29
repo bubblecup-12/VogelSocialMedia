0dccd3f2672ceb1ef718228fce9e02943e5da5da
Niklas <198754647+BlankAccountsUsername@users.noreply.github.com>
2025-06-29 17:29:05 +0200

    reformat Header.tsx, fix index.tsx

--- code/frontend/src/index.tsx
+++ code/frontend/src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { StyledEngineProvider } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StyledEngineProvider } from "@mui/material";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<StyledEngineProvider injectFirst>
-			<App />
+			<StyledEngineProvider injectFirst>
+				<App />
+			</StyledEngineProvider>
 		</StyledEngineProvider>
 	</React.StrictMode>
 );
